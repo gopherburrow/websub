fe428629b9e58a28e9f219b0d0236f63ee71ddac
Tiago van den Berg <tiagovdberg@gmail.com>
2019-02-25 19:39:04 -0300

    Changing method name to make clear that only the signature is validated, not the claims.

--- gosub.go
+++ gosub.go
@@ -43,7 +43,6 @@ var (
 	ErrRequestMustBeNonNil     = errors.New("gosub: request must be non nil")
 	ErrTargetURLMustBeNotEmpty = errors.New("gosub: targetURL must be not empty")
 	ErrTargetURLMustBeValid    = errors.New("gosub: targetURL must be valid")
-	ErrTokenSignatureMustMatch = errors.New("gosub: token signature must match")
 	ErrTokenMissingAudField    = errors.New("gosub: token missing aud field")
 	ErrRequestMustMatchAud     = errors.New("gosub: request must match aud field")
 	ErrTokenMissingIssField    = errors.New("gosub: token missing iss field")
@@ -58,10 +57,6 @@ func SetTransientPage(w http.ResponseWriter) {
 }
 
 type Config struct {
-	//SecretFunc is a function that will be used to create the secret for the HMAC-SHA256 signature for the Web Subroutine JWT Token.
-	//It can be used to rotate the secret, shared the between multiple instances of a Handler or even multiple server instances.
-	//It MUST be non nil and, when called, return NEITHER a nil secret NOR an empty one.
-	Secret func(r *http.Request) []byte
 	//CookieName stores a custom user defined cookie name prefix used to store the WebCall Token.
 	//It can only be set in SetName(name) method.
 	CookieName     string
@@ -69,7 +64,7 @@ type Config struct {
 	TimeoutSeconds int
 }
 
-func (c *Config) Call(w http.ResponseWriter, r *http.Request, targetURL string) error {
+func (c *Config) Call(targetURL string, secret []byte, w http.ResponseWriter, r *http.Request) error {
 	if r == nil {
 		return ErrRequestMustBeNonNil
 	}
@@ -86,24 +81,33 @@ func (c *Config) Call(w http.ResponseWriter, r *http.Request, targetURL string) 
 	}
 	audURL = absolutizeURL(audURL, getScheme(r), r.Host)
 
+	var stateValue string
 	sv := make([]byte, 3)
-	rand.Read(sv)
-	stateValue := base64.RawURLEncoding.EncodeToString(sv)
+	for {
+		rand.Read(sv)
+		stateValue := base64.RawURLEncoding.EncodeToString(sv)
+		cookie, err := r.Cookie(cookieNamePrefix(c) + stateValue)
+		if cookie == nil && err == http.ErrNoCookie {
+			break
+		}
+	}
 
 	q := audURL.Query()
 	q.Set(stateParam(c), stateValue)
 	audURL.RawQuery = q.Encode()
 
-	if err := createTokenCookie(c, w, r, issURL.String(), audURL.String(), stateValue); err != nil {
+	cookie, err := createTokenCookie(c, secret, issURL.String(), audURL.String(), stateValue)
+	if err != nil {
 		return err
 	}
 
+	http.SetCookie(w, cookie)
 	http.Redirect(w, r, audURL.String(), http.StatusTemporaryRedirect)
 	return nil
 }
 
-func (c *Config) Return(w http.ResponseWriter, r *http.Request) error {
-	iss, aud, err := validateAndIssAud(c, r)
+func (c *Config) Return(secret []byte, w http.ResponseWriter, r *http.Request) error {
+	iss, aud, err := validateAndIssAud(c, secret, r)
 	if err != nil {
 		return err
 	}
@@ -130,28 +134,29 @@ func (c *Config) Return(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (c *Config) IsValid(r *http.Request) bool {
-	_, _, err := validateAndIssAud(c, r)
+func (c *Config) IsValid(secret []byte, r *http.Request) bool {
+	_, _, err := validateAndIssAud(c, secret, r)
 	return err != nil
 }
 
-func (c *Config) Refresh(w http.ResponseWriter, r *http.Request) error {
-	iss, aud, err := validateAndIssAud(c, r)
+func (c *Config) Refresh(secret []byte, w http.ResponseWriter, r *http.Request) error {
+	iss, aud, err := validateAndIssAud(c, secret, r)
 	if err != nil {
 		return err
 	}
 
 	stateValue := r.URL.Query().Get(stateParam(c))
-	if err := createTokenCookie(c, w, r, iss, aud, stateValue); err != nil {
+	cookie, err := createTokenCookie(c, secret, iss, aud, stateValue)
+	if err != nil {
 		return err
 	}
-
+	http.SetCookie(w, cookie)
 	return nil
 }
 
 //header("Expires: Sat, 26 Jul 1997 05:00:00
 
-func validateAndIssAud(c *Config, r *http.Request) (iss, aud string, err error) {
+func validateAndIssAud(c *Config, secret []byte, r *http.Request) (iss, aud string, err error) {
 	stateValue := r.URL.Query().Get(stateParam(c))
 
 	cookie, err := r.Cookie(cookieNamePrefix(c) + stateValue)
@@ -163,16 +168,10 @@ func validateAndIssAud(c *Config, r *http.Request) (iss, aud string, err error) 
 	}
 	v := cookie.Value
 
-	//Recover the secret and handle errors.
-	s, err := secret(c, r)
+	//Test if it is ours token (verifying the signature), and not a token created by an attacker.
+	claims, err := jwt.ValidateSignatureHS256(v, secret)
 	if err != nil {
 		return "", "", err
-	}
-
-	//Test if it is ours token (verifying the signature), and not a token created by an attacker.
-	claims, err := jwt.ValidateHS256(v, s)
-	if err != nil {
-		return "", "", ErrTokenSignatureMustMatch
 	}
 
 	audif, ok := (*claims)[jwt.ClaimAudience]
@@ -235,7 +234,7 @@ func stateParam(c *Config) string {
 	return c.StateParam
 }
 
-func createTokenCookie(c *Config, w http.ResponseWriter, r *http.Request, iss, aud, stateValue string) error {
+func createTokenCookie(c *Config, secret []byte, iss, aud, stateValue string) (*http.Cookie, error) {
 	//Create JWT Claims
 	claims := map[string]interface{}{
 		jwt.ClaimIssuer:   iss,
@@ -251,20 +250,14 @@ func createTokenCookie(c *Config, w http.ResponseWriter, r *http.Request, iss, a
 		claims[jwt.ClaimExpirationTime] = jwt.NumericDate(time.Now().Add(time.Duration(timeout) * time.Second))
 	}
 
-	//Recover the secret and handle errors.
-	s, err := secret(c, r)
+	t, err := jwt.CreateHS256(claims, secret)
 	if err != nil {
-		return err
-	}
-
-	t, err := jwt.CreateHS256(claims, s)
-	if err != nil {
-		return err
+		return nil, err
 	}
 
 	audURL, err := url.Parse(aud)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	cookie := &http.Cookie{
@@ -279,20 +272,5 @@ func createTokenCookie(c *Config, w http.ResponseWriter, r *http.Request, iss, a
 		cookie.MaxAge = timeout
 	}
 
-	http.SetCookie(w, cookie)
-	return nil
-}
-
-func secret(c *Config, r *http.Request) ([]byte, error) {
-	//Retrieve the Token secret (Custom or generated). Handle errors.
-	if c.Secret == nil {
-		return nil, ErrSecretError
-	}
-	secret := c.Secret(r)
-	//It is part of the contract. Never return an empty secret. (Because it is no secret that way.)
-	if secret == nil || len(secret) == 0 {
-		return nil, ErrSecretError
-	}
-
-	return secret, nil
+	return cookie, nil
 }
